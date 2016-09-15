13150a83560a4978494cdc55a1b6de8ddb077150
RameshDhawane <rameshdhawane1993@gmail.com>
2016-09-15 23:08:48 +0530

    Bike Curd Operations

--- services/services.php
+++ services/services.php
@@ -11,9 +11,20 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
             addOwners($myPostData->ownerDetails);
         }
     }
+
+    if(isset($myPostData->bikeDetails)){
+        if(isset($myPostData->bikeDetails->id)){
+            updateBike($myPostData->bikeDetails);
+        }else{
+            addBike($myPostData->bikeDetails);
+        }
+    }
 }else if($_SERVER['REQUEST_METHOD'] == "GET"){
     if(isset($_GET['ownerId'])){
         deleteOwner($_GET['ownerId']);
+    }
+    if(isset($_GET['bikeId'])){
+        deleteBike($_GET['bikeId']);
     }
 }
 mysql_close($connection);
@@ -51,6 +62,71 @@ function deleteOwner($id){
 
     $query = "DELETE FROM `main_database`.`bike_owner` WHERE id='$id'";
     $result = mysql_query($query);
+    if($result){
+        echo  "Done! Owner Deleted";
+    }else{
+        echo  "ERROR! No Such Owner Exist";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function addBike($bikeData){
+    $query = "INSERT INTO `main_database`.`bike` (`id`, `name`, `description`, `number_plate`, `image`, `chasisnumber`, `rate_per_hr`, `bike_owner_id`) VALUES (NULL, '$bikeData->name', '$bikeData->description','$bikeData->number_plate','$bikeData->image','$bikeData->chasisnumber','$bikeData->rate_per_hr',NULL );";
+    $result = mysql_query($query);
+    if($result){
+        echo  "Done! Bike added Sucessfully";
+    }else{
+        echo  "ERROR! Cannot add  Bike";
+    }
+}
+
+function updateBike($bikeData){
+    $query = "select * from `main_database`.`bike` where id='$bikeData->id'";
+    $result = mysql_query($query);
+    $queryData = mysql_fetch_object($result);
+    if($queryData){
+        $query = "update `main_database`.`bike` set name='$bikeData->name',description ='$bikeData->description',number_plate='$bikeData->number_plate',image='$bikeData->image',chasisnumber='$bikeData->chasisnumber',rate_per_hr='$bikeData->rate_per_hr',bike_owner_id='$bikeData->bike_owner_id' where id='$bikeData->id'";
+        $result = mysql_query($query);
+        if($result){
+            echo  "Done! Owner Updated Sucessfully";
+        }else{
+            echo  "ERROR! Cannot Update Owner";
+        }
+    }
+}
+
+function deleteBike($id){
+
+    $query = "DELETE FROM `main_database`.`bike` WHERE id='$id'";
+    $result = mysql_query($query);print_r($result);
     if($result){
         echo  "Done! Owner Deleted";
     }else{
